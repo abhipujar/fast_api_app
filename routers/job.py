56fa6598194f74719c71ccecdab4821145f312ba
abhipujar <4al23ai037@aiet.org.in>
2026-06-29 10:26:37 +0530

    github

--- routers/job.py
+++ routers/job.py
@@ -1,31 +1,51 @@
-from fastapi import APIRouter
-from schemas.job import JobCreate, JobUpdate
+from fastapi import APIRouter,HTTPExeption,Depends,status
+from schemas.job import JobCreate, JobUpdate, JobResponse
+from models.job import Job
+from sqlalchemy.orm import Session
+from database import get_db
 
 router = APIRouter(prefix="/job", tags=["job"])
 jobs = []
 
-@router.post("/")
-def create_job(job: JobCreate):
-    jobs.append(job)
-    return jobs
+@router.post("/",status_code=status.HTTP_201_CREATED,response_model=JobResponse)
+def create_job(job: JobCreate, db: Session = Depends(get_db)):
+    db_job = Job(**job.dict())
+    db.add(db_job)
+    db.commit()
+    db.refresh(db_job)
+    return db_job
 
-@router.get("/")
+@router.get("/",status_code=status.HTTP_200_OK,response_model=list[JobResponse])
 def get_all_job():
+    jobs = db.query(Job).all()
     return jobs
 
 @router.get("/{job_id}")
 def get_job(job_id: int):
-    return jobs[job_id]
+    jobs = db.query(Job).filter(Job.id == job_id).first()
+    if not job:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Job with id {job_id} not found")
+    return job
 
-@router.put("/{job_id}")
-def update_job(job_id: int, job: JobUpdate):
-    jobs[job_id] = job
-    return jobs
+@router.put("/{job_id}",status_code=status.HTTP_200_OK)
+def update_job(job_id: int, job: JobUpdate, db: Session = Depends(get_db)):
+    db_job = db.query(Job).filter(Job.id == job_id).first()
+    if not db_job:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Job with id {job_id} not found")
+    for key, value in job.dict().items():
+        setattr(db_job, key, value)
+    db.commit()
+    db.refresh(db_job)
+    return db_job
 
-@router.delete("/{job_id}")
-def delete_job(job_id: int):
-    jobs.pop(job_id)
-    return jobs
+@router.delete("/{job_id}",status_code=status.HTTP_200_OK)
+def delete_job(job_id: int, db: Session = Depends(get_db)):
+    db_job = db.query(Job).filter(Job.id == job_id).first()
+    if not db_job:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Job with id {job_id} not found")
+    db.delete(db_job)
+    db.commit()
+    return {"message": f"Job with id {job_id} deleted successfully"}
 
 # @router.get("/")
 # def read_job():
