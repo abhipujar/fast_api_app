d4a03d0a72ce950c793573ef32e4b10d6f97faf4
abhipujar <4al23ai037@aiet.org.in>
2026-06-29 16:44:51 +0530

    github

--- frontend/talentspark/src/App.tsx
+++ frontend/talentspark/src/App.tsx
@@ -1,10 +1,16 @@
 import Welcome from './components/welcome';
 import NavBar from './components/NavBar';
+import CompanyCard from './components/CompanyCard';
+import JobCard from './components/JobCard';
+import Footer from './components/footer';
 function App() {
   return (
       <>
           <NavBar />
           <Welcome />
+          <CompanyCard />
+          <JobCard />
+          <Footer />
       </>
   )
 }
